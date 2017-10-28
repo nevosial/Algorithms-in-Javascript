5aa16319e8c3d899c542cd2eea648c5e43614242
Neville Mascarenhas <nevosial@gmail.com>
2017-10-28 08:37:34 -0700

    recursive fibonacci

---
+++ misc/fibonacci.js
@@ -0,0 +1,11 @@
+//Fibonacci series using recursion.
+
+function fibonacci(n) {
+    if(n <= 2) {
+        return 1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+};
+
